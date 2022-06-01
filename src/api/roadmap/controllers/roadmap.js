8e5ce578ffa4159e44460a30665f52b92ff3ad32
Vainius <vmykolaitis1@gmail.com>
2022-06-01 16:34:25 +0300

    Added Roadmap schema

---
+++ src/api/roadmap/controllers/roadmap.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  roadmap controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::roadmap.roadmap');
---
+++ src/api/roadmap/services/roadmap.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * roadmap service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::roadmap.roadmap');
